72fec9184eaeda1e83ba64dfaa9ef6a911222b86
elijah <rrrolento@yahoo.com>
2018-04-19 09:29:57 -0700

    test

--- lab2/src/lab2.js
+++ lab2/src/lab2.js
@@ -21,46 +21,54 @@ var FSIZE = 4
 // the number we will be using to model pi
 const pi = 3.1459
 
+// refrence for the rubber band line
 let previousX = null
 let previousY = null 
+
+// g_points = current working line, oldlines = all finished lines (an array of arrays)
 let g_points = [] // The array for the position of a mouse press
 let oldlines = [] // all previous completed lines
+
+// width = current width, oldwidths = all old widths
 let width = 1.0 //current working width (thickness of line)
 let oldwidths = [] //all old widths
+
+// (x)Color = current color setting, oldcolors = all old colors (an array of arrays)
 let Rcolor = 1
 let Gcolor = 0
 let Bcolor = 0
 let Acolor = 1
 let oldcolors = [] // array of arrays. X = one entry. X[1]=R X[2] = G X[3] = B X[4] = A
-let formvalue = -1 
-let sizeofpoint = 10.0 
-let cylinder_points = []
-
 const defaultcolor = []
 defaultcolor.push(Rcolor)
 defaultcolor.push(Gcolor)
 defaultcolor.push(Bcolor)
 defaultcolor.push(Acolor)
 
+// formvalue = for game, size of point = default point size
+let formvalue = -1 
+let sizeofpoint = 10.0 
+
+// cylinder_points = currently drawing cylinder points
+// sides = number of size the cylinder will have
+let cylinder_points = []
+let sides = 10
+let radius = 0.25
+
 function main() {
   // Retrieve <canvas> element
   const canvas = document.getElementById('webgl');
 
-// old lab1 stuff
 
 //  const sliderSize = document.getElementById('newslider')
   const sliderR = document.getElementById('sliderR')
   const sliderG = document.getElementById('sliderG')
   const sliderB = document.getElementById('sliderB')
   const sliderA = document.getElementById('sliderA')
-//  const textbox = document.getElementById('textbox')
-//  const button = document.getElementById('button')
-//  const shiftL = document.getElementById('shiftL')
-//  const shiftR = document.getElementById('shiftR')
-//  const shiftU = document.getElementById('shiftU')
-//  const shiftD = document.getElementById('shiftD')
-//  const pointSize = document.getElementById('pointslider')
-//  const gameStart = document.getElementById('gamebutton')
+  const textDelete = document.getElementById('textDelete')
+  const buttonDelete = document.getElementById('buttonDelete')
+  const sliderSides = document.getElementById('sliderSides')
+  const sliderRadius = document.getElementById('sliderRadius')
 
   // Get the rendering context for WebGL
   var gl = getWebGLContext(canvas);
@@ -89,27 +97,20 @@ function main() {
   canvas.onmousemove = function(ev){ move(ev, gl, canvas, a_Position); };
   canvas.onkeydown = function(ev){ key(ev, gl, canvas, a_Position); };
 
-// old, lab1 Functions !
-//  sliderSize.oninput = function(ev){ slide(ev, gl, canvas, sliderSize,  a_Position); };
+//  sliderSize.oninput = function(ev){ pointThick(ev, gl, canvas, sliderSize,  a_Position); };
   sliderR.oninput = function(ev){ Rslider(ev, gl, canvas, sliderR,  a_Position); };
   sliderG.oninput = function(ev){ Gslider(ev, gl, canvas, sliderG,  a_Position); };
   sliderB.oninput = function(ev){ Bslider(ev, gl, canvas, sliderB,  a_Position); };
   sliderA.oninput = function(ev){ Aslider(ev, gl, canvas, sliderA,  a_Position); };
-//  button.onclick = function(ev){ keypress(ev, gl, canvas, textbox,  a_Position); };
-//  shiftL.onclick = function(ev){ shiftPointsL(ev, gl, canvas,  a_Position); };
-//  shiftR.onclick = function(ev){ shiftPointsR(ev, gl, canvas,  a_Position); };
-//  shiftU.onclick = function(ev){ shiftPointsU(ev, gl, canvas,  a_Position); };
-//  shiftD.onclick = function(ev){ shiftPointsD(ev, gl, canvas,  a_Position); };
-//  pointSize.oninput = function(ev){ adjustpointsize(ev, gl, canvas, pointSize,a_Position); };
-//  gameStart.onclick = function(ev){ startgame(ev, gl, canvas,  a_Position); };  
+  buttonDelete.onclick = function(ev){ deleteCylinder(ev, gl, canvas, textDelete,  a_Position); };
+  sliderSides.oninput = function(ev){ changeSides(ev, gl, canvas, sliderSides,  a_Position); };
+  sliderRadius.oninput = function(ev){ changeRadius(ev, gl, canvas, sliderRadius,  a_Position); };
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
  
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT); 
-
-
 }
 
 function leftclick(ev, gl, canvas, a_Position) {  
@@ -363,7 +364,7 @@ function drawAllCylinders(gl,canvas,a_Position){
   }  
 }
 
-function slide(ev, gl, canvas, sliderSize, a_Position) { 
+function pointThick(ev, gl, canvas, sliderSize, a_Position) { 
   width=sliderSize.value
 }
 
@@ -383,8 +384,8 @@ function Aslider(ev, gl, canvas, sliderA, a_Position) {
   Acolor=sliderA.value
 }
 
-function keypress(ev, gl, canvas, textbox, a_Position) { 
-  let  deletenumber = parseInt(textbox.value)
+function deleteCylinder(ev, gl, canvas, textDelete, a_Position) { 
+  let  deletenumber = parseInt(textDelete.value)
   if (isNaN(deletenumber)){
    return
   }
@@ -440,183 +441,17 @@ function updateColor (gl,a_Position,R,G,B,A){
   }
 }
 
-// shift left 
-function shiftPointsL(ev, gl, canvas, a_Position) {  
-  //shift current line 
-  if (previousX !== null)
-  previousX = previousX - 0.1 
-  for(var i = 0; i < g_points.length; i += 2) { 
-    g_points[i]=g_points[i] - 0.1
-  }
-  if (oldlines.length > 0){
-   for (var f =0 ; f < oldlines.length ; f++){       
-     for(var j = 0; j < oldlines[f].length; j += 2) { 
-       oldlines[f][j] = oldlines[f][j] - 0.1
-     }
-   } 
- }
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT); 
-  // draw all old lines (if they exist) 
-  if (oldlines.length > 0){
-    for (var i =0 ; i < oldlines.length ; i++){       
-      draw (gl,canvas,a_Position,oldlines[i],oldwidths[i],oldcolors[i])
-    } 
-  }
- var vertices = new Float32Array(g_points)
- // draw currently working line with points
- const colors = []
- colors.push(Rcolor)
- colors.push(Gcolor)
- colors.push(Bcolor)
- colors.push(Acolor)
- draw (gl,canvas,a_Position,vertices,width,colors)
-}
-
-// shift right
-function shiftPointsR(ev, gl, canvas, a_Position) {  
-  //shift current line 
-  if (previousX !== null)
-  previousX = previousX + 0.1 
-  for(var i = 0; i < g_points.length; i += 2) { 
-    g_points[i]=g_points[i] + 0.1
-  }
-  if (oldlines.length > 0){
-   for (var f =0 ; f < oldlines.length ; f++){       
-     for(var j = 0; j < oldlines[f].length; j += 2) { 
-       oldlines[f][j] = oldlines[f][j] + 0.1
-     }
-   } 
- }
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT); 
-  // draw all old lines (if they exist) 
-  if (oldlines.length > 0){
-    for (var i =0 ; i < oldlines.length ; i++){       
-      draw (gl,canvas,a_Position,oldlines[i],oldwidths[i],oldcolors[i])
-    } 
-  }
- var vertices = new Float32Array(g_points)
- // draw currently working line with points
- const colors = []
- colors.push(Rcolor)
- colors.push(Gcolor)
- colors.push(Bcolor)
- colors.push(Acolor)
- draw (gl,canvas,a_Position,vertices,width,colors)
-}
-
-// shift up 
-function shiftPointsU(ev, gl, canvas, a_Position) {  
-  //shift current line 
-  if (previousY !== null)
-  previousY = previousY + 0.1 
-  for(var i = 0; i < g_points.length; i += 2) { 
-    g_points[i+1]=g_points[i+1] + 0.1
-  }
- if (oldlines.length > 0){
-   for (var f =0 ; f < oldlines.length ; f++){       
-     for(var j = 0; j < oldlines[f].length; j += 2) { 
-       oldlines[f][j+1] = oldlines[f][j+1] + 0.1
-     }
-   } 
- }
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT); 
-  // draw all old lines (if they exist) 
-  if (oldlines.length > 0){
-    for (var i =0 ; i < oldlines.length ; i++){       
-      draw (gl,canvas,a_Position,oldlines[i],oldwidths[i],oldcolors[i])
-    } 
-  }
- var vertices = new Float32Array(g_points)
- // draw currently working line with points
- const colors = []
- colors.push(Rcolor)
- colors.push(Gcolor)
- colors.push(Bcolor)
- colors.push(Acolor)
- draw (gl,canvas,a_Position,vertices,width,colors)
-}
-
-// shift down
-function shiftPointsD(ev, gl, canvas, a_Position) {  
-  //shift current line 
-  if (previousY !== null)
-  previousY = previousY - 0.1 
-  for(var i = 0; i < g_points.length; i += 2) { 
-    g_points[i+1]=g_points[i+1] - 0.1
-  }
- if (oldlines.length > 0){
-   for (var f =0 ; f < oldlines.length ; f++){       
-     for(var j = 0; j < oldlines[f].length; j += 2) { 
-       oldlines[f][j+1] = oldlines[f][j+1] - 0.1
-     }
-   } 
- }
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT); 
-  // draw all old lines (if they exist) 
-  if (oldlines.length > 0){
-    for (var i =0 ; i < oldlines.length ; i++){       
-      draw (gl,canvas,a_Position,oldlines[i],oldwidths[i],oldcolors[i])
-    } 
-  }
- var vertices = new Float32Array(g_points)
- // draw currently working line with points
- const colors = []
- colors.push(Rcolor)
- colors.push(Gcolor)
- colors.push(Bcolor)
- colors.push(Acolor)
- draw (gl,canvas,a_Position,vertices,width,colors)
-}
-
-function adjustpointsize(ev, gl, canvas, pointsize, a_Position) { 
-  sizeofpoint = parseFloat(pointsize.value)
-}
-
-function startgame(ev, gl, canvas, a_Position) { 
-  //clear canvas
-  previousX = null
-  previousY = null 
-  g_points = [] // The array for the position of a mouse press
-  oldlines = [] // all previous completed lines
-  width = 1.0 //current working width (thickness of line)
-  oldwidths = [] //all old widths
-  Rcolor = 1
-  Gcolor = 0
-  Bcolor = 0
-  Acolor = 1
-  oldcolors = [] // array of arrays. X = one entry. X[1]=R X[2] = G X[3] = B X[4] = A
-  formvalue = -1 
-  sizeofpoint = 10.0 
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
- 
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT); 
-  setTimeout (check,20000)
-}
-
-function check(){
-  if (oldlines.length < 5)
-   console.log ("\n\n\n\n\n\n\n\n\n\n\nyou failed")
-  else
-   console.log ("\n\n\n\n\n\n\n\n\n\n\nyou passed")
-}
-
 function drawcylinder(gl,canvas,a_Position,x1,y1,x2,y2,colors){
   // multiply degrees by convert to get value in radians
   const convert = pi/180
-  const radius = 0.25
+  const test = Math.floor(360/sides)
   // gets x,y for circle
-  for (var i=0 ; i <=360 ; i+=36){ 
+  for (var i=0 ; i <=360 ; i+=test){ 
     cylinder_points.push(Math.cos(convert*i) * radius + x1)
     cylinder_points.push(Math.sin(convert*i) * radius + y1)
     cylinder_points.push(0)
   } 
-  for (var i=0 ; i <=360 ; i+=36){ 
+  for (var i=0 ; i <=360 ; i+=test){ 
     cylinder_points.push(Math.cos(convert*i) * radius + x2)
     cylinder_points.push(Math.sin(convert*i) * radius + y2)
     cylinder_points.push(0)
@@ -624,4 +459,12 @@ function drawcylinder(gl,canvas,a_Position,x1,y1,x2,y2,colors){
   var vertices = new Float32Array(cylinder_points)
   // draw currently working line with points
  drawGeneralizedCylinder (gl,canvas,a_Position,vertices,width,colors)
+}
+
+function changeSides(ev, gl, canvas, sliderSides,  a_Position){
+  sides = sliderSides.value
+}
+
+function changeRadius(ev, gl, canvas, sliderRaidus,  a_Position){
+  radius = sliderRadius.value
 }
